cb7da93904068fad2c76f89b1638013703bcd118
EhabNasr <ehab.mohamed.nasr@gmail.com>
2017-04-21 05:37:32 +0200

    toggle

--- Control.js
+++ Control.js
@@ -1,11 +1,7 @@
 /**
  * Created by ehab on 4/21/2017.
  */
-function LightsOn() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "CommandHandler.php?on", true);
-    xhttp.send();
-}
+
 function LightsToggle() {
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "CommandHandler.php?T", true);
