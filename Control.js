d8edd59673aaf82ad4c1cf654809e755a156ecb9
EhabNasr <ehab.mohamed.nasr@gmail.com>
2017-04-21 22:41:36 +0200

    AJAX Tasks

--- Control.js
+++ Control.js
@@ -1,21 +1,25 @@
 /**
  * Created by ehab on 4/21/2017.
  */
-
-function lightsToggle() {
+function Handle(url, cFunction){
     var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cFunction(this);
+        }
+    };
+}
+function lightsToggle() {
     xhttp.open("GET", "CommandHandler.php?T", true); //T for Toggle lights
     xhttp.send();
 }
 
-function checkLights() {
-    var xhttp = new XMLHttpRequest();
+function checkLights(xhttp) {
     xhttp.open("GET", "CommandHandler.php?C", true); //C for CheckLights
     xhttp.send();
 }
 
-function lockDoor() {
-    var xhttp = new XMLHttpRequest();
+function lockDoor(xhttp) {
     xhttp.open("GET", "CommandHandler.php?LD", true); //LD for LockDoor
     xhttp.send();
 }
